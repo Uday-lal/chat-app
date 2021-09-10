cf6ef95796f8110218f2efa0085810595caf10cc
Uday-lal <udaylal014@gmail.com>
2021-09-10 10:03:32 -0700

    Add flash message to the login page

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, session
 from .form import EnterForm
 from . import SOCKET_IO
 from flask_socketio import emit
+from flask import flash
 
 views = Blueprint("views", __name__)
 connected_users = []
@@ -12,15 +13,20 @@ def home():
     enter_form = EnterForm()
     if enter_form.validate_on_submit():
         username = enter_form.username.data
-        connected_users.append(username)
-        session["username"] = username
-        return redirect(url_for("views.chat"))
+        if username not in connected_users:
+            connected_users.append(username)
+            session["username"] = username
+            return redirect(url_for("views.chat"))
+        flash("This name is already taken use a different and unique name")
+        return redirect(url_for("views.home"))
     return render_template("index.html", enter_form=enter_form)
 
 
 @views.route("/chat")
 def chat():
-    return render_template("chat.html", peoples=connected_users, connected_users=len(connected_users))
+    return render_template("chat.html",
+                           peoples=connected_users,
+                           connected_users=len(connected_users))
 
 
 @SOCKET_IO.on("message")
@@ -41,4 +47,5 @@ def handle_connect():
     emit("connection_state", {
         "username": username,
         "connected_users": len(connected_users)
-    }, broadcast=True)
+    },
+         broadcast=True)
