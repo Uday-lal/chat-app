3a38b74e6479c970bb5c643768ff29701ae98ac8
Uday-lal <udaylal014@gmail.com>
2021-09-15 23:55:25 +0530

    Update website/__init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -7,20 +7,6 @@ APP = Flask(__name__)
 SOCKET_IO = SocketIO(APP)
 
 
-@APP.context_processor
-def override_url_for():
-    return dict(url_for=dated_url_for)
-
-
-def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
-        if filename:
-            file_path = os.path.join(APP.root_path, endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
-
-
 def start_app():
     from .views import views
     APP.config["SECRET_KEY"] = secrets.token_hex(28)
